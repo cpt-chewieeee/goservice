7ead9d52e6b74849bf1b534363463a2567c37339
cpt-chewieeee <hzhan016@gmail.com>
2017-10-15 00:14:10 -0700

    finish setting up hot reloader for golang :D

--- service/server.go
+++ service/server.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"log"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:])
+func handler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:], "\n")
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func withName(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func main() {
 	fmt.Println("_____API_SERVICES_STARTED_____")
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	router := httprouter.New()
+	// router.GET("/", handler)
+	router.GET("/test/:name", withName)
+
+	// http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
